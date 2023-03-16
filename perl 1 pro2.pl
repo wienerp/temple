e3b3a97e6beed0ea86f543f6e6551dcf92635d70
wienerp <candy.bca2ndyr@gmail.com>
2023-03-16 01:22:26 -0700

    first commit

---
+++ perl 1 pro2.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+print"Please enter your name\n";
+my $name
+chomp($name);
+print"$name please enter your age\n",my $age=<>;
+chomp($age);
+print"You are $age years old\n";
+print $age 18?"$name is adult";$name is storage"; 
